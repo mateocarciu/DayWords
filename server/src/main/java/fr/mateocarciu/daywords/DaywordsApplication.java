db485c55376eccfcfe04743b3fea9b4a41af2ea3
Matthieu Flce <flacelierematthieu@gmail.com>
2024-08-12 14:56:00 +0200

    init server base

---
+++ server/src/main/java/fr/mateocarciu/daywords/DaywordsApplication.java
@@ -0,0 +1,13 @@
+package fr.mateocarciu.daywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaywordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaywordsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/fr/mateocarciu/daywords/DaywordsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mateocarciu.daywords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaywordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
