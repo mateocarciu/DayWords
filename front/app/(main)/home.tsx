8cd7447ee2456fa0656f394f771570a89f73a108
Mateo <carciu98@gmail.com>
2025-03-23 19:05:39 +0100

    fix(fetch-entries): fetch entries everytime user navigates to home

--- front/app/(main)/home.tsx
+++ front/app/(main)/home.tsx
@@ -1,17 +1,25 @@
 import FloatingButton from '@/components/buttons/FloatingButton'
 import ScreenWrapper from '@/components/ScreenWrapper'
-import { useRouter } from 'expo-router'
+import { useRouter, useFocusEffect } from 'expo-router'
 import EntryList from '@/components/entries/EntryList'
 import HomeHeader from '@/components/HomeHeader'
+import { useCallback, useState } from 'react'
 
 const home = () => {
 	const router = useRouter()
+	const [refreshKey, setRefreshKey] = useState(0)
+
+	useFocusEffect(
+		useCallback(() => {
+			setRefreshKey((prev) => prev + 1)
+		}, [])
+	)
 
 	return (
 		<ScreenWrapper scrollEnabled={true}>
 			<HomeHeader />
 			<FloatingButton onPress={() => router.push('/newPost')} />
-			<EntryList />
+			<EntryList key={refreshKey} />
 		</ScreenWrapper>
 	)
 }
